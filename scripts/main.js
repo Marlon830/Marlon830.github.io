583589f7b1006838325cc319d303f443bf7677cb
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-01-24 11:09:05 +0100

    salutatousjesuisfiaiof afjap jfpaefpoaeofjapoefjopaejfoajfpojaej  aeipaiejpjepofjpoajpozjpojzpoejopjpojpojpojpojpospojpojaposdjpoazrjiehifahipfsad asd

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
+var i = 1;
+alert('la premiere');
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Réussi ! Hello world !';
+alert('wow quelle alerte là');
+alert('la deuxieme');
+document.querySelector('html').addEventListener('click', function() {
+    alert('Tu as cliqué ' + i + ' fois.');
+    i = i + 1;
+});
