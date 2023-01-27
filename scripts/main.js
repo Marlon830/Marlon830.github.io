efab6692a19de5efe05b8147912fbc4aebbc90a8
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-01-27 10:01:25 +0100

    updated portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
-var i = 1;
-alert('la premiere');
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Réussi ! Hello world !';
-alert('wow quelle alerte là');
-alert('la deuxieme');
-document.querySelector('html').addEventListener('click', function() {
-    alert('Tu as cliqué ' + i + ' fois.');
-    i = i + 1;
-});
+onMounted(async () => {
+    const handleOnMouseMove = e => {
+        const { currentTarget: target } = e;
+
+        const rect = target.getBoundingClientRect()
+        let    x = e.clientX - rect.left
+        let    y = e.clientY - rect.top;
+
+        target.style.setProperty("--mouse-x", `${x}px`);
+        target.style.setProperty("--mouse-y", `${y}px`);
+    }
+
+    for (const card of document.querySelectorAll(".card")) {
+        card.onmousemove = e => handleOnMouseMove(e);
+    }
+})
