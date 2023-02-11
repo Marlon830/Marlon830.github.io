093b4e6061be83802f5b6e01a4ffd561c0ce41d2
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-11 21:21:55 +0100

    added more and better responsivity to the site, now the scale of the website changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,20 @@ window.onload = function() {
          card.style.setProperty("--mouse-x", `${x}px`);
          card.style.setProperty("--mouse-y", `${y}px`);
        };
-     } 
+     }
+    resizeWindow();
    }
+
+function resizeWindow()
+{
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  var fac = (h / 900);
+
+  if (w <= 1080 && h <= 900) {
+    document.getElementById("cards").style.transform = `scale(${fac})`;
+  } else {
+    document.getElementById("cards").style.transform = 'scale(1)';
+  }
+  setTimeout(resizeWindow, 200);
+}
